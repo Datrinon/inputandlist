d8caa31c8c8d61f9341b972ff1ca526cf2b7f28f
Datrinon <dtrinh089@gmail.com>
2021-10-10 09:22:56 -0700

    Add ability to add tasks, which the view updates accordingly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import './App.css';
+import react, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Overview from './components/Overview';
+
+const TASKS = [
+  "Do the dishes",
+  "Take out the garbage",
+  "Go grocery shopping."
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {tasks: TASKS};
+  }
+
+  onTaskSubmit(e) {
+    e.preventDefault();
+    console.log("firing callback from app...");
+    
+    let task = document.querySelector(".task-add-field").value;
+    if (task != null) {
+      let currentTasks = this.state.tasks;
+      this.setState({tasks: currentTasks.push(task)})
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Task Overview </h1>
+        <Overview tasks={TASKS} submitHandler={this.onTaskSubmit.bind(this)}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,1 +1,35 @@
-import react, {component} from 'react'
+import react, {Component} from 'react'
+
+const Input = ({submitHandler}) => {
+  return (
+    <form onSubmit={submitHandler}>
+      <input id="task-add" className="task-add-field"
+        type="text" placeholder="click here to add a task"/>
+      <button>Add Task</button>
+    </form>
+  );
+}
+
+class Overview extends Component {
+  render() {
+    const taskElements = [];
+    this.props.tasks.forEach((task, index) => {
+      let taskView = (
+        <li className="task">{task}</li>
+      );
+
+      taskElements.push(taskView);
+    })
+
+    return (
+      <div className="overview">
+        <ol className="tasks">
+          {taskElements}
+        </ol>
+        <Input submitHandler={this.props.submitHandler} />
+      </div>
+    )
+  }
+}
+
+export default Overview;
